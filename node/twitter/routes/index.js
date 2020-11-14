b17b46a0609b04a36e8f60bb2e2c905616a9a55c
M_Bedy <marcelin.bedy@gmail.com>
2020-11-14 18:33:09 +0100

    node: twitter - partie 2 - leçon 3 Création du formulaire pour l'ajout d'un tweet

--- node/twitter/routes/index.js
+++ node/twitter/routes/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_, res) => {
   res.render("tweets/tweet-list");
+});
+
+router.get("/tweet/new", (_, res) => {
+  res.render("tweets/tweet-form");
 });
 
 module.exports = router;
